b71548bc61fc3e0a6a922fb49d0d8f760c939362
Cesaar Valdez <valdezcervera.info@gmail.com>
2019-09-30 10:21:41 +0200

    before moving folders

--- db.js
+++ db.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
 // eslint-disable-next-line no-console
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => console.log(`Connected with ${process.env.MONGODB_URI}`)) // eslint-disable-line no-console
-  .catch(error => console.log(error)); // eslint-disable-line no-console
+
+const monk = require('monk');
+
+// const db = monk(process.env.MONGODB_URL);
+
+// mongoose.connect('mongo/karma', { useNewUrlParser: true })
+//   .then(() => console.log(`Connected with ${process.env.MONGODB_URI}`)) // eslint-disable-line no-console
+//   .catch(error => console.log(error)); // eslint-disable-line no-console
 
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const cors = require('koa-cors');
 const router = require('./routers/router');
 
 const app = new Koa();
-const { PORT } = process.env || 8080;
+const PORT = process.env.PORT || 3000;
+
 
 // override koa's undocumented error hanlder
 app.context.onerror = errorHandler;
 // specify that is is our api
 app.context.api = true;
+console.log('mountedddd');
 
 // middlewares
 app
@@ -23,7 +25,7 @@ app
   .use(bodyParser())
   .use(router.routes());
 
-
+console.log('6465468486');
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is listening on port ${PORT}🤘`);
